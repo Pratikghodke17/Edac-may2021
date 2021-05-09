75e81217029e69aacb5d87a29123afbfd8c6eb14
Pratikghodke17 <83281139+Pratikghodke17@users.noreply.github.com>
2021-05-09 22:35:30 +0530

    Add files via upload

---
+++ Assignment2/star3.java
@@ -0,0 +1,30 @@
+/*
+*****
+****
+***
+**
+*
+
+*/
+public class star3
+{
+    public static void main(String args[])  
+    { 
+        int i, j,num; 
+    
+        for(i=1; i<=5; i++)
+        { 
+            num=1; 
+            for(j=5; j>=i; j--)
+            { 
+                 
+                System.out.print("*"); 
+    
+            } 
+    
+            System.out.println(); 
+        } 
+    } 
+       
+   
+}
