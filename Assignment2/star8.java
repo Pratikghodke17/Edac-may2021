b9dab9213adf1aed5106e5c96885d03db920bb7f
Pratikghodke17 <83281139+Pratikghodke17@users.noreply.github.com>
2021-05-09 23:10:37 +0530

    Add files via upload

---
+++ Assignment2/star8.java
@@ -0,0 +1,29 @@
+public class star8
+{ 
+ public static void main(String args[]) 
+    { 
+        int i, j;  
+        for(i=1; i<=5; i++)  
+		{ 
+            for(j=1; j<=i; j++)
+            {       
+                System.out.print("*"); 
+            }           
+            System.out.println(); 
+        }
+
+    
+        for(i=2; i<=5; i++)
+        { 
+            
+            for(j=5; j>=i; j--)
+            { 
+                 
+                System.out.print("*"); 
+    
+            } 
+    
+            System.out.println(); 
+        } 
+    } 		
+    } 
