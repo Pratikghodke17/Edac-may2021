14ae78d72835209290c43bb780bf3f3846a6f910
Pratikghodke17 <83281139+Pratikghodke17@users.noreply.github.com>
2021-05-10 00:11:56 +0530

    Add files via upload

---
+++ Assignment2/star10.java
@@ -0,0 +1,19 @@
+public class star10
+{ 
+  public static void main(String args[])
+    { 
+        int i, j;  
+        for(i=1; i<=5; i++)
+		{ 
+		    for (j=4; j>=i; j--)
+            {           
+                System.out.print(" ");
+            } 
+            for(j=1; j<=5; j++)
+            {       
+                System.out.print("*");
+            }
+			System.out.println();
+        } 
+    } 
+}
